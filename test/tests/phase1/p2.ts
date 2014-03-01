a9e7b530a2dd97852133f67b4f04b560bbd5ee9b
david <dtk24@wildcats.unh.edu>
2014-03-01 00:02:43 -0500

    multiple var statements without initialization

---
+++ test/tests/phase1/p2.ts
@@ -0,0 +1,13 @@
+// print = function (x) { console.log(x); };
+
+var i;
+var j;
+var k;
+
+i = true;
+j = false;
+k = 4;
+
+print (i * j);
+print (i * j + k + 38);
+
---
+++ test/tests/phase1/p8.ts
@@ -0,0 +1,9 @@
+// print = function (x) { console.log(x); };
+
+var i = 1, j = 2, k = 4;
+
+print i;
+print j;
+print k;
+
+print (i + j * k + 33);
